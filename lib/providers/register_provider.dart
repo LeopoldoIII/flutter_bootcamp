18bd33a690e7bf01759b4cccd77c32dbff9ca786
LeopoldoIII <nathan.rx@gmail.com>
2022-11-19 20:06:37 -0600

    register page

--- lib/providers/register_provider.dart
+++ lib/providers/register_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class RegisterProvider extends ChangeNotifier {
   RegisterProvider() {
@@ -6,6 +9,13 @@ class RegisterProvider extends ChangeNotifier {
   }
 
   Future<bool> registerUser(Map<String, String> formData) async {
+    var url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=');
+
+    var response = await http.post(url, body: jsonEncode(formData));
+    if (response.statusCode == 200) {
+      return true;
+    }
     return false;
   }
 }
