cf52f9ee60447bf732b6e69d7f4f905e9e5e0749
LeopoldoIII <nathan.rx@gmail.com>
2023-02-19 12:29:56 -0600

    profile data

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -13,13 +13,60 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   UserProvider userProvider = UserProvider();
+  Map<String, String> formData = {
+    'email': '',
+    'password': '',
+    'name': '',
+    'lastname': '',
+  };
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     userProvider = Provider.of<UserProvider>(context);
+
+    nameController.text = userProvider.user.name!;
+    lastNameController.text = userProvider.user.lastname!;
+
     return Scaffold(
       appBar: getAppBar(context, 'Profile', userProvider.user),
       drawer: AppDrawer(),
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            AppFormFiled(
+              'name',
+              'Nombre',
+              controller: nameController,
+              icon: Icons.verified_user_sharp,
+              formData: formData,
+              validator: (value) {
+                if (value!.length < 4) {
+                  return "Nombre no valido";
+                }
+                return null;
+              },
+            ),
+            AppFormFiled(
+              'lastname',
+              'Last name',
+              controller: lastNameController,
+              icon: Icons.verified_user_sharp,
+              formData: formData,
+              validator: (value) {
+                if (value!.length < 4) {
+                  return "Nombre no valido ";
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Actualizar'))
+          ],
+        ),
+      ),
     );
   }
 }
